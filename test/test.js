f7bf8c831927e3ade93ffe499b04f4013d641b93
Jasper van de Gronde <th.v.d.gronde@hccnet.nl>
2016-08-15 15:40:25 +0200

    Corrected path in test.js. Expected.jpeg was not prefixed with a path, even though it is in the test subdirectory. This caused a problem when running the tests.

--- test/test.js
+++ test/test.js
@@ -143,7 +143,7 @@ tap("save-pixels saving a RGB png", function(t) {
 tap("save-pixels saving a RGB jpeg", function(t) {
   var x = zeros([64, 64, 3])
   var actualFilepath = "temp.jpeg"
-  var expectedFilepath = "expected.jpeg"
+  var expectedFilepath = "test/expected.jpeg"
 
   for(var i=0; i<64; ++i) {
     for(var j=0; j<64; ++j) {
